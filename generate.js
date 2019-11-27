d67a5145ee8d9646acab6d8b47f7d56d6b190c0c
Javier Blanco García-Lomas <jblancogl@gmail.com>
2019-11-27 09:18:43 +0100

    test graph viz with 3d-force-graph

--- generate.js
+++ generate.js
@@ -2,25 +2,24 @@ const fg = require('fast-glob')
 const path = require('path')
 const utils = require('./utils.js')
 const fs = require('fs').promises
-const Graph = require("@dagrejs/graphlib").Graph
-const dot = require('graphlib-dot')
 
-async function main() {
-  const dataFolder = path.join(process.cwd(), 'data', '_data')
+async function main () {
+  const dataFolder = path.join(process.cwd(), 'data')
   const isDataFolderCreated = await utils.fsExists(dataFolder)
 
   if (!isDataFolderCreated) {
     throw new Error('Data folder is not yet created')
   }
 
-  const g = new Graph()
   const files = await fg(`${dataFolder}/**.json`)
+  const nodes = []
+  const edges = []
 
   for (const file of files) {
     const json = await fs.readFile(file)
     const data = await JSON.parse(json)
 
-    g.setNode(data.username)
+    nodes.push(data.username)
   }
 
   for (const file of files) {
@@ -28,13 +27,16 @@ async function main() {
     const data = await JSON.parse(json)
 
     for (let record of data.following) {
-      if (g.hasNode(record)) {
-        g.setEdge(record, data.username)
+      if (nodes.includes(record)) {
+        edges.push({ from: record, to: data.username })
       }
     }
   }
 
-  console.log(dot.write(g))
+  console.log(JSON.stringify({
+    nodes: nodes,
+    edges: edges
+  }))
 }
 
 main(process)
